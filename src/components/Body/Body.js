e0cfab126a1277676e7da6a886f8ecf4ed08fed8
Aman Rawat <amanrawa2@gmail.com>
2021-12-11 15:09:38 +0530

    initial commit

---
+++ src/components/Body/Body.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../Navbar/Navbar";
+import "./Body.css";
+
+const Body = () => {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+};
+
+export default Body;
